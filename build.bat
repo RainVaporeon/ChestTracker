af99408ce0917365a5a37b1eb9929ec1fe9cc7f6
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-03-24 19:59:09 +0800

    1.1.0

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+title Building...
+gradlew clean build
+echo Built project can be found in build\libs
+pause
---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+title ChestTracker by OriTheSpirit - [Exit: Ctrl-C]
+if not exist ChestAPI-1.1.0.jar (
+echo ChestAPI-1.1.0.jar not found, did you put the file in the same directory?
+pause
+goto :EOF
+)
+java -jar ChestAPI-1.1-0.jar
